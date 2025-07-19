3634e282c89bfab37adf17e4a47d068481132abf
Rajdeep Karmakar <rajdeepkarmakar2027@gmail.com>
2025-07-19 22:35:31 +0530

    refactor: Remove image optimization configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
